54f0c4ff0e3008f6d25cd376db0ab04de642ea40
Changjun Kim <beaican530@gmail.com>
2021-06-08 19:44:21 +0900

    Improve time complexity

--- inflearn/implementation/4-최대나이차이.cpp
+++ inflearn/implementation/4-최대나이차이.cpp
@@ -1,19 +1,17 @@
-// 브루트포스: O(n)
+// 최댓값 - 최댓값 : O(n)
 
 #include <iostream>
 using namespace std;
 int a[105];
 int main(){
-    int n, max = 0;
+    int n, max = -2147000000, min = 2147000000;
     cin >> n;
     for(int i = 0; i < n; i++)
         cin >> a[i];
-    for(int i = 0; i < n-1; i++){
-        for(int j = 1; j < n; j++){
-            if(max < abs(a[i]-a[j]))
-                max = abs(a[i]-a[j]);
-        }
+    for(int i = 0; i < n; i++){
+        if(max < a[i]) max = a[i];
+        if(min > a[i]) min = a[i];
     }
-    cout << max;
+    cout << max - min;
 }
 
