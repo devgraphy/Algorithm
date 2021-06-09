3649b3c6f9549c65d3e15e857a89c56b6b8ee670
Changjun Kim <beaican530@gmail.com>
2021-06-09 18:09:22 +0900

    wrong answer(boundary error)

---
+++ inflearn/implementation/18-층간소음.cpp
@@ -0,0 +1,28 @@
+// 경계값 넘는 최대 구간 길이 구하기
+// wrong answer
+#include <iostream>
+using namespace std;
+int main(){
+    int n, m, temp;
+    int cnt = 0, max = -2147000000;
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        cin >> temp;
+        if(temp > m){
+            //cout << temp << " ";
+            cnt++;
+            //cout << cnt << " ";
+        }
+        else{
+            if(max < cnt){
+                max = cnt;
+                cout << max << " ";
+            }
+            cnt = 0;
+        }
+    }
+    if(max > 0)
+        cout << max;
+    else
+        cout << -1;
+}
