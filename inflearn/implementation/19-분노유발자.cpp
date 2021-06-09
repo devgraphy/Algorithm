cf534b9f8d78ff1de75a93da17564f23c7eedd75
Changjun Kim <beaican530@gmail.com>
2021-06-09 18:44:45 +0900

    wrong answer(boundary value)

---
+++ inflearn/implementation/19-분노유발자.cpp
@@ -0,0 +1,26 @@
+// 브루트포스 O(n^2) - 최대 입력 100 - 1만번 연산 ok
+// wrong answer
+#include <iostream>
+using namespace std;
+int a[101];
+int main(){
+    int n;
+    int chk = 0;
+    int cnt = 0;
+    cin >> n;
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    for(int i = 0; i < n-1; i++){
+        chk = 0;
+        for(int j = i + 1; j < n;j++){
+            if(a[i] < a[j]){
+                chk = 1;
+                break;
+            }
+        }
+        if(chk == 0)
+            cnt++;
+    }
+    cout << cnt;
+}
