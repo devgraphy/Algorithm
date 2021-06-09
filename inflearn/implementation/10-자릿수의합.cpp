9623e90d502186425ccab0abf5e3ca0cf433ba49
Changjun Kim <beaican530@gmail.com>
2021-06-09 11:02:32 +0900

    treat as number

--- inflearn/implementation/10-자릿수의합.cpp
+++ inflearn/implementation/10-자릿수의합.cpp
@@ -2,27 +2,29 @@
 #include <string>
 using namespace std;
 int main(){
-    int n;
+    int n, x;
     int temp = 0;
     int max = -2147000000;
     int max_p = 0;
-    string str[101];
+    int num[101];
     cin >> n;
     for(int i = 0; i < n; i++)
-        cin >> str[i];
+        cin >> num[i];
     for(int i = 0; i < n; i++){
-        for(int j = 0; j < str[i].length();j++){
-            temp+= (str[i][j]-'0');
-        }
+        x = num[i];
+        while(x > 0){
+            temp = temp + (x % 10);
+            x/=10;
+        } 
         if(max < temp){
             max = temp;
             max_p = i;
         }
         if(max == temp){
-            if(stoi(str[max_p])<stoi(str[i]))
+            if(num[max_p]<num[i])
                 max_p = i;
         }
         temp = 0;
     }
-    cout << str[max_p];
+    cout << num[max_p];
 }
