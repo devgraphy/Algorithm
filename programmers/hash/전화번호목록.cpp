13aa039be8a308243b992cd986622aa4c612ec4e
Changjun Kim <beaican530@gmail.com>
2021-06-20 10:08:40 +0900

    refer to solution(hash)

--- programmers/hash/전화번호목록.cpp
+++ programmers/hash/전화번호목록.cpp
@@ -5,6 +5,8 @@
 알고리즘(2for - bruteforce):O(n^2) - 시간 초과!
 --------------------<Improve time complexity>--------------------
 알고리즘(문자열 배열의 정렬은 사전식 나열임을 이용):O(n)
+--------------------<Approach in a different way>--------------------
+알고리즘(해쉬 이용):O(n)
 */
 /* 채점 결과
 정확성: 83.3
@@ -15,17 +17,25 @@
 #include <vector>
 #include <iostream>
 #include <string>
-#include <algorithm>
+#include <unordered_map>
 using namespace std;
 
 bool solution(vector<string> phone_book) {
     bool answer = true;
     int sz = phone_book.size();
     int subsz;
-    sort(phone_book.begin(),phone_book.end());
-    for(int i = 0; i < sz-1; i++){
+    string str="";
+    unordered_map<string, int> hash_map;
+    for(int i = 0; i < sz; i++){
         subsz = phone_book[i].length();
-        if(phone_book[i] == phone_book[i+1].substr(0,subsz))
+        str="";
+        for(int j = 0; j < subsz; j++){
+            str+=phone_book[i][j];
+            hash_map[str]++;
+        }
+    }
+    for(int i = 0; i < sz; i++){
+        if(hash_map[phone_book[i]] > 1 )
             return false;
     }
     return answer;
