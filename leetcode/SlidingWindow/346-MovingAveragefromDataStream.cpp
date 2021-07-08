f4a1aaa7803db481bc4880048069755adbc76358
Changjun Kim <beaican530@gmail.com>
2021-07-08 13:48:24 +0900

    solve myself(math-no traverse all for each window)

--- leetcode/SlidingWindow/346-MovingAveragefromDataStream.cpp
+++ leetcode/SlidingWindow/346-MovingAveragefromDataStream.cpp
@@ -18,31 +18,44 @@
 Runtime: 36 ms, faster than 15.84% of C++ online submissions for Moving Average from Data Stream.
 Memory Usage: 14 MB, less than 14.78% of C++ online submissions for Moving Average from Data Stream.
 */
+/* -----<생각 던지기>-----
+next호출할 때마다 매번 window 크기만큼 매번 계산을 다시 해야할까?
+- 이미 window 사이즈보다 넘치는 경우는 기존 기존 sum에 새로운 val을 추가하고 이전 winsize의 크기 안에 들어오는 원소 중 가장 먼저 들어온 원소를 빼주면 되지 않을까?
+- 이미 window 사이즈만큼 안 차있는 경우에는 기존 sum에 그냥 더해주면 된다.
+=> math를 이용하여 sliding window를 구현하자.
+*/
+/* ----------<시간개선 알고리즘:o(call)>----------
+math
+
+1. 벡터에 값 추가
+2. double 타입의 sum 정의(실수로 표현하려면 반드시 연산자 중 하나는 실수 타입이어야 한다.)
+3. 벡터 크기가 window size보다 크면(윈도우가 한 칸 이동하면서 합산에서 제외해야하는 원소가 발생한다)
+    현재 원소부터 window 크기를 지정했을 때, 바로 직전의 원소를 sum에서 빼고, 새로오 들어온 값을 더해준다.
+4. 작거나 같으면
+    sum에 새로운 원소만 더한다.
+
+Runtime: 20 ms, faster than 88.73% of C++ online submissions for Moving Average from Data Stream.
+Memory Usage: 14 MB, less than 14.78% of C++ online submissions for Moving Average from Data Stream.
+*/
 class MovingAverage {
 public:
     /** Initialize your data structure here. */
     int winsize;
     vector<int> a;
-    MovingAverage(int size) {   // 사이즈 크기 입력
+    double sum = 0;
+    MovingAverage(int size) {
         winsize = size;
     }
-    
-    double next(int val) {   // 입력 인자에 대해서 슬라이딩 윈도우 크기만큼 평균값 계산
+
+    double next(int val) {
         a.push_back(val);
-        
-        if(a.size() >= winsize){
-            double sum = 0;
-            for(int i = 0; i < winsize;i++){
-                sum += a[a.size()-i-1];
-            }
-            return sum/winsize;
+        if(a.size() > winsize){ // window 크기보다 크면 기존 원소 하나를 버린다.
+            sum += a[a.size()-1] - a[a.size()-winsize-1];
+            return sum / winsize;
         }
-        else{
-            double sum = 0;
-            for(int i = 0; i < a.size();i++)
-                sum += a[i];
-            return sum/a.size();
+        else{   // 작거나 같으면 sum에 새로운 val을 추가하여 계산한다.
+            sum+=a[a.size()-1];
+            return sum / a.size();
         }
-        
     }
 };
