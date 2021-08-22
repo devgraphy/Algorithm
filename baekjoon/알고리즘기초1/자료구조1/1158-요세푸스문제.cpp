1560c51e554147239a3444255f36079ed4692f80
Changjun Kim <beaican530@gmail.com>
2021-08-22 12:18:30 +0900

    refer to solution(circular linked list)

--- baekjoon/알고리즘기초1/자료구조1/1158-요세푸스문제.cpp
+++ baekjoon/알고리즘기초1/자료구조1/1158-요세푸스문제.cpp
@@ -1,49 +1,44 @@
 /* ----------<문제 정의>----------
-문제: 입력 순서를 유지하면서 중간 요소를 삭제하는 문제(queue)
+문제: 입력 순서를 유지하면서 중간 요소를 삭제하는 문제(queue, circular linked list)
 입력: N, K
 출력: (N,K) 요세푸스 순열 출력
-*/
-/* ----------<알고리즘>----------
-풀이 시간: 20분
-메모리:2020KB
-시간:68ms
-
-큐를 이용해서 입력 순서를 유지하면서 중간 요소 삭제하기
-큐에 1 ~ n 삽입
-(큐를 하나만 이용해보자)
-첫 번째 - k-1 번째 수까지 큐에 다시 삽입, k 번째는 pop (4 5 6 7 1 2)
-두 번째 - 첫번째 큐상태에서 k-1번째 수까지 큐에 다시 삽입, k 번째는 pop (7 1 2 4 5)
-...
 */
 /* ----------<아이디어>----------
 환형 순서의 데이터에서 중간 삭제가 빠른 자료구조 -> 환형 연결 리스트로 구현 가능?
 */
+/* ----------<알고리즘>----------
+환형연결리스트로 순서 유지하면서 삽입, 삭제 O(1) 구현
+메모리:2152KB
+시간:96ms
+*/
+/* -----<주의할 점>-----
+환형 연결리스트를 구현하고 싶을 때,
+    (X) if(pos == ll.end()) pos++;
+    (O) if(pos == ll.end()) pos = ll.begin();
+
+위 작업을 iterator를 증가할 때마다 반드시 해줘야 한다.
+*/
 #include <iostream>
-#include <queue>
+#include <list>
 using namespace std;
 int main(){
     int n, k;
-    queue<int> q;
+    list<int> ll;
     cin >> n >> k;
-    // 1 ~ n 순서대로 큐에 삽입
+    // 1~n 수열로 리스트 만들기
     for(int i = 1; i <= n; i++)
-        q.push(i);
+        ll.push_back(i);
+    auto pos = ll.begin();  // 첫 번째 원소를 가리킨다.
     cout << '<';
-    // 문자열 형식 때문에 어쩔 수 없는 코드 중복
-    for(int j = 0; j < k-1; j++){
-            q.push(q.front());
-            q.pop();
+    // 형식에 맞게 반복 출력
+    while(ll.size() > 1){
+        for(int i = 0; i < k-1; i++){
+            pos++;
+            if(pos == ll.end()) pos = ll.begin();   // 환형리스트 구현 시, end->begin으로 대체
         }
-    cout << q.front();
-    q.pop();
-    for(int i = 0; i < n-1; i++){   // 앞에서 한번 수행했으므로 n-1번
-        // 앞에서부터 k-1번째 수까지 큐 뒤에 삽입
-        for(int j = 0; j < k-1; j++){
-            q.push(q.front());
-            q.pop();
-        }
-        cout << ", "<< q.front();
-        q.pop();
+        cout << *pos << ", " ;
+        pos = ll.erase(pos);
+        if(pos == ll.end()) pos = ll.begin();
     }
-    cout << '>';
+    cout << *pos << '>';
 }
