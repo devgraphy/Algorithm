27488441351f07d99665c43b14678cfdd5f48f55
Changjun Kim <beaican530@gmail.com>
2021-08-29 16:48:40 +0900

    Refactoring(Removed duplicate code)

--- baekjoon/알고리즘기초1/자료구조1/17413-단어뒤집기2.cpp
+++ baekjoon/알고리즘기초1/자료구조1/17413-단어뒤집기2.cpp
@@ -4,23 +4,17 @@
 출력: 문자열
 주의 사항: 구현
 */
-/* ----------<알고리즘>----------
-bool tag 정의 해서 <가 들어왔을 때, tag = true
-    tag가 true이면 문자열 단순 출력
->가 들어오면 tag = false
+/* ----------<리팩터드 알고리즘>----------
+- stack pop 코드 중복 제거
+- 문자 기준에서 flip true/false 기준 알고리즘으로 변경
 
-뒤집기를 해야 하는 시점 판단은 공백 또는 <
-*주의: 문자열 끝은 공백이 아니므로 임의로 추가해줘야 한다.
-
-문자가 
-< 인 경우, 그대로 출력 tag=true
-> 인 경우, 그대로 출력 tag=false
-이 외의 문자가 들어온 경우
-    tag = true인 경우 - 문자든 공백이든 그대로 출력
-    tag = false인 경우 - 문자면 스택에 푸시, 공백이면 스택에 있는 것 모두 출력
-
-메모리:2268KB
-시간:8ms
+flip = false인 경우
+    그냥 출력
+    < : 스택 요소 모두 pop, flip = false, 그대로 출력
+    > : flip = true, 그대로 출력
+flip = true인 경우
+    stack에 푸시
+    공백일 때 출력
 */
 #include <iostream>
 #include <string>
@@ -32,35 +26,23 @@ int main(){
     stack<char> st;
     getline(cin, str);
     str+=' ';
-    int strlen = str.length();
-    // cout << strlen << endl;
-    for(int i = 0; i < strlen; i++){
-        if(str[i] == '<') {
-            while(!st.empty()){
-                cout << st.top();
-                st.pop();
-            }
-            flip = false;
-            cout << '<';
-        }
-        else if(str[i] == '>') {
-            flip = true; 
-            cout << '>';
-        }
-        else{  // 그 외 문자
-            if(flip == true){   // 괄호와 상관없는 문자
-                if(str[i] == ' '){
-                    while(!st.empty()){
-                        cout << st.top();
-                        st.pop();
-                    }
-                    cout << ' ';
+    for(auto c : str){
+        if(flip == true){
+            if(c == ' ' || c == '<'){
+                while(!st.empty()){
+                    cout << st.top();
+                    st.pop();
                 }
-                else    st.push(str[i]);
+                cout << c;
+                if(c == '<') flip = false;
             }
-            else{               // <>괄호 안의 문자
-                cout << str[i];
+            else{   // 그 외 문자
+                st.push(c);
             }
+        }
+        else if(flip == false){
+            cout << c;
+            if(c == '>') flip = true;
         }
     }
 }
