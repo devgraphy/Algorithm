a4b98a73939a3cb4a3b312447df0987242614d6e
Changjun Kim <beaican530@gmail.com>
2021-09-03 23:45:37 +0900

    refactoring(array->vector), resolve #34

--- baekjoon/알고리즘기초1/자료구조1/10828-스택.cpp
+++ baekjoon/알고리즘기초1/자료구조1/10828-스택.cpp
@@ -6,15 +6,19 @@
 /* ----------<자료구조 선택 근거>----------
 배열(명령어가 최대 10만이므로 최대 입력 10만개로 가정해서 배열 크기를 잡는다.)
 */
+/* ----------<자료구조 변경>----------
+배열의 고정 크기로 인한 문제를 해결하기 위해 벡터로 바꾼다.
+*/
 #include<iostream>
 #include<string>
+#include<vector>
 using namespace std;
 int main(){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int n, num, top = -1;
-    int st[10001]={0,};
+    int n, num;
+    vector<int> st;
     string comm;
     cin >> n;
     
@@ -23,26 +27,26 @@ int main(){
         cin >> comm;
         if(comm == "push"){
             cin >> num;
-            st[++top] = num;
+            st.push_back(num);
         }
         else if(comm == "pop"){
-            if(top>=0){
-                cout << st[top] << endl;
-                st[top--] = 0;
+            if(!st.empty()){
+                cout << st.at(st.size()-1) << endl;
+                st.pop_back();
             }
             else cout << -1 << endl;
 
         }
         else if(comm == "size"){
-            cout << top + 1 << endl;
+            cout << st.size() << endl;
         }
         else if(comm == "empty"){
-            if(top >= 0) cout << 0 << endl;
+            if(!st.empty()) cout << 0 << endl;
             else cout << 1 << endl;
 
         }
         else if(comm == "top"){
-            if(top >= 0) cout << st[top] << endl;
+            if(!st.empty()) cout << st.at(st.size()-1) << endl;
             else cout << -1 << endl;
             
         }
