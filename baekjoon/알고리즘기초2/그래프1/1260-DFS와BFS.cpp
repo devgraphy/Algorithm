df221d76bbba1a58a784a30b16c54ecdd52330b6
Changjun Kim <beaican530@gmail.com>
2021-09-11 08:06:32 +0900

    solve myself(bfs using queue)

--- baekjoon/알고리즘기초2/그래프1/1260-DFS와BFS.cpp
+++ baekjoon/알고리즘기초2/그래프1/1260-DFS와BFS.cpp
@@ -8,10 +8,12 @@
 - 또는 계속 해제하기 때문에 또 다시 방문하는 것이다.
 */
 #include <iostream>
+#include <queue>
 using namespace std;
 int n, m, v;
 int a[1001][1001];
 int chk[1001];
+queue<int> q;
 void dfs(int v){
     chk[v] = 1;
     cout << v << " ";
@@ -22,8 +24,22 @@ void dfs(int v){
         dfs(i);
     }
 }
-void bfs(int tv){
-
+void bfs(int v){
+    q.push(v);
+    chk[v] = 1;
+    int temp;
+    while(!q.empty()){
+        // v와 연결된 정점 추가
+        temp = q.front();
+        q.pop();
+        cout << temp << " ";
+        for(int i = 1; i <= n; i++){
+            if(a[temp][i] == 1 && chk[i] == 0){
+                q.push(i);
+                chk[i] = 1; // q에 넣는 시점에 방문 체크(O) vs q에서 꺼내는 시점에 방문 체크(X)-꺼내는 동안 다른 정점에 의해 중복방문
+            }
+        }
+    }
 }
 int main(){
     cin >> n >> m >> v;
@@ -34,5 +50,9 @@ int main(){
         a[temp2][temp1] = 1;
     }
     dfs(v);
-    //bfs(v);
+    cout << "\n";
+    for(int i = 0; i <= n; i++){    // 초기화
+        chk[i] = 0;
+    }
+    bfs(v);
 }
