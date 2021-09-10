d7520dd2e57580b9842ba5ae79f9004a46f37d38
Changjun Kim <beaican530@gmail.com>
2021-09-11 01:54:46 +0900

    solve myself(counterexample:revisit due to canceling visit check), resolve #44

--- baekjoon/알고리즘기초2/그래프1/1260-DFS와BFS.cpp
+++ baekjoon/알고리즘기초2/그래프1/1260-DFS와BFS.cpp
@@ -1,5 +1,11 @@
 /* ----------<문제 정의>----------
 문제: DFS, BFS 구현
+입력: (입력으로 주어지는 간선은 양방향이다.)
+*/
+/* -----<반례>-----
+더 이상 방문할 수 있는 점이 없는 경우 종료한다.
+- 종료 시점을 정해주지 않았다.
+- 또는 계속 해제하기 때문에 또 다시 방문하는 것이다.
 */
 #include <iostream>
 using namespace std;
@@ -11,9 +17,9 @@ void dfs(int v){
         if(a[v][i] == 0) continue;  // 연결 정보 확인
         if(chk[i] == 1) continue;
         chk[i] = 1;
+        // cout << v << " : " << i << endl; // dfs 흐름을 알 수 있는 테스트 코드
         cout << i << " ";
         dfs(i);
-        chk[i] = 0;
     }
 }
 void bfs(int tv){
@@ -27,9 +33,8 @@ int main(){
         a[temp1][temp2] = 1;
         a[temp2][temp1] = 1;
     }
-    cout << v << " ";
     chk[v] = 1;
+    cout << v << " ";
     dfs(v);
-    chk[v] = 0;
     //bfs(v);
 }
