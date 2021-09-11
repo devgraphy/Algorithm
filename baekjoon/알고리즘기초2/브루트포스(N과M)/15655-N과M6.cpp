0b845287f5823fc100d87ba66d2ce59728ae43af
Changjun Kim <beaican530@gmail.com>
2021-09-11 18:33:48 +0900

    solve myself(combination, no duplicate selection)

---
+++ baekjoon/알고리즘기초2/브루트포스(N과M)/15655-N과M6.cpp
@@ -0,0 +1,44 @@
+/* ----------<문제 정의>----------
+문제: 조합(중복x)
+입력: n, m(최대 10)
+*/
+/* ----------<접근법>----------
+dfs bruteforce, for-idx, visit check
+dfs(cnt, idx)
+    cnt - 선택 개수이면서 방문 순서 정보를 가짐
+    for idx 정보 사용 <- 앞선 정보를 기준으로 만들 수 있는 모든 경우의 수를 만들어 나가므로 제외
+        visit   <- 중복 방문 방지
+        dfs
+        visit cancle
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, m;
+int a[10];  // 입력 정보
+int chk[10];    // 방문 회피 목적
+int comb[10];   // 반복문 횟수 절감 목적
+void dfs(int cnt, int idx){
+    if(cnt == m){
+        for(int i = 0; i<m;i++){
+            cout << comb[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i = idx; i < n;i++){
+        if(chk[i] == 0){
+            chk[i] = 1;
+            comb[cnt] = a[i];
+            dfs(cnt+1, i+1);
+            chk[i] = 0;
+        }
+    }
+}
+int main(){
+    cin >> n >> m;
+    for(int i = 0; i < n; i++)
+        cin >> a[i];
+    sort(a, a+n);
+    dfs(0, 0);
+}
