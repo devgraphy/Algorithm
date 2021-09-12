8ab3275a65bcb40e75158cd368e290d5c52c171b
Changjun Kim <beaican530@gmail.com>
2021-09-12 16:30:24 +0900

    counterexamples exist -#55

---
+++ baekjoon/알고리즘기초2/브루트포스(N과M)/15663-N과M9.cpp
@@ -0,0 +1,43 @@
+/* ----------<문제 정의>----------
+문제: 중복된 수가 포함된 n개의 수 + 순열(중복된 수는 다른 수로 취급하여 하나의 수열에 표현할 수 있지만, 똑같은 수열이 중복해서 나타날 수 없다.) 
+입력: n, m(최대 8)
+출력: 중복 수열 없이 출력
+
+주의사항: 중복된 수는 다른 수로 취급하여 하나의 수열에 표현할 수 있지만, 똑같은 수열이 중복해서 나타날 수 없다.
+*/
+/* ----------<접근법>----------
+순열
+*/
+#include <iostream>
+#include <algorithm>
+using namespace std;
+int n, m;
+int a[10];
+int chk[10];
+int perm[10];
+
+void dfs(int cnt){
+    if(cnt == m){
+        for(int i = 0; i < m;i++){
+            cout << perm[i] << " ";
+        }
+        cout << '\n';
+        return;
+    }
+    for(int i = 0; i < n; i++){
+        if(chk[i] == 0){
+            chk[i] = 1;
+            perm[cnt] = a[i];
+            dfs(cnt+1);
+            chk[i] = 0;
+        }
+    }
+}
+int main(){
+    cin >> n >> m;
+    for(int i = 0; i < n; i++){
+        cin >> a[i];
+    }
+    sort(a, a+n);
+    dfs(0);
+}
