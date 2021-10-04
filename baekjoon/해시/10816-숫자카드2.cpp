f2dce39b1cdb96656032ff2eca79732811d4d306
Changjun Kim <beaican530@gmail.com>
2021-10-04 23:32:25 +0900

    refer to solution(cin, cout optimization), resolved #75

--- baekjoon/해시/10816-숫자카드2.cpp
+++ baekjoon/해시/10816-숫자카드2.cpp
@@ -5,13 +5,22 @@
 */
 /* ----------<접근법>----------
 마이너스 범위와 플러스 범위를 고려하여 2천개의 배열 자료구조를 이용할 수 있지만(O(1)에 접근)
-공간 복잡도를 줄이기 위해 해시맵을 사용한다.
-O(N+M+M)
+공간 복잡도를 줄이기 위해 맵을 사용한다.
+*/
+/* ----------<모르는 것>----------
+map자료구조는 해시가 아니라 이진트리로 구현되어 있다.
+그래서 O(N+M+M)이 아니라
+이진 탐색 트리의 경우 삽입/삭제/찾기 연산 각각에 대해 모두 평균 시간복잡도는 O(logN), 최악의 시간복잡도는 O(N)
+그래서 결국 O(NlogN + MlogM) 이다.
+시간 초과 날만 하다..
 */
 #include <iostream>
 #include <map>
 using namespace std;
 int main(){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
     int n, m, temp;
     map<int, int> card_m;
     cin >> n;
@@ -22,6 +31,9 @@ int main(){
     cin >> m;
     for(int i = 0; i < m; i++){
         cin >> temp;
-        cout << card_m[temp] << ' ';
+        if(card_m.find(temp) != card_m.end())
+            cout << card_m.find(temp)->second << " ";
+        else
+            cout << 0 << " ";
     }
 }
