1519e8729d0315b846ad2cfbc264e757ad100d67
Changjun Kim <beaican530@gmail.com>
2021-10-16 15:45:21 +0900

    solve myself(recursion)

---
+++ baekjoon/브루트포스/10870-피보나치수5.cpp
@@ -0,0 +1,31 @@
+/* ----------<문제 정의>----------
+문제: n번째 피보나치 수 구하기
+*/
+/* ----------<접근법>----------
+재귀 사용법 익힐 겸 재귀를 이용한다.
+*/
+/* ----------<알고리즘>----------
+        3
+    2       1
+0       1   
+
+    int fib(n)
+    if(n==0 ||n == 1){
+        return n;
+    }
+    return fib(n-2) + fib(n-1);
+
+    main
+    배열에 인덱스 0과 1의 원소에 각각 0, 1을 할당
+*/
+#include <iostream>
+using namespace std;
+int fib(int n){
+    if(n == 0 || n == 1) return n;
+    return fib(n-2) + fib(n-1);
+}
+int main(){
+    int n;
+    cin >> n;
+    cout << fib(n);
+}
