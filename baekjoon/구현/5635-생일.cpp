61fcb24ebcde8b736a19c3bbfcf28bc3ca1bc218
Changjun Kim <beaican530@gmail.com>
2021-10-16 14:10:46 +0900

    counterexamples exist -#87

--- baekjoon/구현/5635-생일.cpp
+++ baekjoon/구현/5635-생일.cpp
@@ -8,6 +8,8 @@
 한 문자열에서 이름 일 월 연도의 위치가 다르다.
 벡터의 배열 또는 배열의 배열로 정의한다.
 연도부터 비교해야하므로 비트 연산자의 동작 방식을 알고 있어야 한다.
+연도가 같은 경우 다른 경우를 구분해야 한다.
+    또 월이 같은 경우 다른 경우를 구분해야 한다.
 for n
     for 4
         최솟값 비교(일, 월, 연도 모두 작다면)
@@ -36,21 +38,56 @@ int main(){
     for(int i = 0; i < n; i++){
         // 한 라인의 정보 입력 받기
         cin >> tempname >> tempdd >> tempmm >> tempyy;
-        // 최솟값 갱신
+        // 나이가 많은 사람 갱신
         //cout << tempname << tempdd << tempmm << tempyy << endl;
-        if(tempyy <= minbirth[2] && tempmm <= minbirth[1] && tempdd <= minbirth[0]){
+        if(tempyy == minbirth[2]){
+            if(tempmm == minbirth[1]){   // 월까지 같은 경우
+                if(tempdd < minbirth[0]){
+                    minbirth[0] = tempdd;
+                    minbirth[1] = tempmm;
+                    minbirth[2] = tempyy;
+                    minname = tempname;
+                }
+            }
+            else if(tempmm < minbirth[1]){
+                minbirth[0] = tempdd;
+                minbirth[1] = tempmm;
+                minbirth[2] = tempyy;
+                minname = tempname;
+            }
+            
+        }
+        else if(tempyy < minbirth[2]){
             minbirth[0] = tempdd;
             minbirth[1] = tempmm;
             minbirth[2] = tempyy;
             minname = tempname;
-            cout << minname << endl;
         }
-        if(tempyy >= maxbirth[2] && tempmm >= maxbirth[1] && tempdd >= maxbirth[0]){
-            minbirth[0] = tempdd;
-            minbirth[1] = tempmm;
-            minbirth[2] = tempyy;
+        // 나이가 적은 사람 갱신
+        if(tempyy == maxbirth[2]){
+            if(tempmm == maxbirth[1]){   // 월까지 같은 경우
+                if(tempdd > maxbirth[0]){
+                    maxbirth[0] = tempdd;
+                    maxbirth[1] = tempmm;
+                    maxbirth[2] = tempyy;
+                    maxname = tempname;
+                }
+            }
+            else if(tempmm > maxbirth[1]){
+                maxbirth[0] = tempdd;
+                maxbirth[1] = tempmm;
+                maxbirth[2] = tempyy;
+                maxname = tempname;
+            }
+            
+        }
+        else if(tempyy > minbirth[2]){
+            maxbirth[0] = tempdd;
+            maxbirth[1] = tempmm;
+            maxbirth[2] = tempyy;
             maxname = tempname;
-        }      
+        }
+            
     }
     cout << maxname << '\n' << minname;
 }
